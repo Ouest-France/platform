3ad1162750e965245a61499d0d1d6cd8a1fef91c
François-Guillaume RIBREAU <github@fgribreau.com>
2017-12-20 15:21:51 +0100

    fix(BlockConfig): add support for mustache

--- packages/schemas/BlockConfig.test.js
+++ packages/schemas/BlockConfig.test.js
@@ -1,7 +1,9 @@
 describe('BlockConfig', () => {
   it('is exposed as a validation function', () => {
     expect(
-      require('.').getSchema('https://raw.githubusercontent.com/Ouest-France/platform/master/packages/schemas/BlockConfig.json')
+      require('.').getSchema(
+        'https://raw.githubusercontent.com/Ouest-France/platform/master/packages/schemas/BlockConfig.json'
+      )
     ).toBeInstanceOf(Function);
   });
 
@@ -40,6 +42,7 @@ describe('BlockConfig', () => {
             {
               name: 'default',
               labels: [{ key: 'theme', value: 'default' }],
+              engine: 'mustache',
               source:
                 '<section class="sd-bloc sd-meteo widget">    <h2 class="sd-meteo-title">        {{ title }}    </h2>    <div class="wnc_encart">        <a href="{{ link }}">            <div class="wnc_encart_row">                <div class="prevision"><span class="temp">{{ temp }}</span>                    <svg class="wnc_ty1" version="1.1" xmlns="http://www.w3.org/2000/svg"                         width="{{ svg.width }}"                         height="{{ svg.height }}"                         viewBox="{{ svg.view_box }}">                        <title>{{ svg.title }}</title>                        <path d="{{ svg.path }}"></path>                    </svg>                    <span class="hour">{{ hour }}</span></div>                <div class="description">                    <p class="localite">                        {{ city }}                    </p>                    <p>La météo détaillée<br/>de votre localité<br/>heure par heure</p>                    <p class="maree">                        <span class="ico-mer"></span> {{ tide_status }} : <strong>{{ tide }}</strong>                        Coef :                        <strong>{{ tide_coefficient }}</strong>                    </p>                </div>            </div>            <p class="baseline">Prévisions à 10 jours <span class="ico-chevron_right"></span></p>        </a>    </div></section>',
               assets: {
